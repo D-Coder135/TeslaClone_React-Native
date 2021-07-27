9e31ba40ec82e8058571932605ab4e26f7a8d50a
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-27 21:56:59 +0530

    add some reformatting to the code

--- App.js
+++ App.js
@@ -3,56 +3,57 @@ import React from 'react';
 import {StyleSheet, Text, View, ImageBackground} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
+    return (
+        <View style={styles.container}>
 
-      <ImageBackground
-          source={require('assets/images/ModelX.jpeg')}
-          style={styles.image}
+            <ImageBackground
+                source={require('assets/images/ModelX.jpeg')}
+                style={styles.image}
+            />
 
-      <View style={styles.carContainer}>
+            <View style={styles.carContainer}>
 
-        <View style={styles.titles}>
-          <Text style={styles.title}>
-            Model S
-          </Text>
-          <Text style={styles.subtitle}>
-            Starting at $69,428
-          </Text>
+                <View style={styles.titles}>
+                    <Text style={styles.title}>
+                        Model S
+                    </Text>
+                    <Text style={styles.subtitle}>
+                        Starting at $69,428
+                    </Text>
+                </View>
+
+            </View>
+            <StatusBar style="auto"/>
         </View>
-
-      </View>
-      <StatusBar style="auto"/>
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 
-  carContainer: {
-    width: '100%',
-    height: '100%',
-  },
+    carContainer: {
+        width: '100%',
+        height: '100%',
+    },
 
-  titles: {
-    marginTop: '30%',
-    width: '100%',
-    alignItems: 'center',
-  },
+    titles: {
+        marginTop: '30%',
+        width: '100%',
+        alignItems: 'center',
+    },
 
-  title: {
-    fontSize: 40,
-    fontWeight: "500",
-  },
+    title: {
+        fontSize: 40,
+        fontWeight: "500",
+    },
 
-  subtitle: {
-    fontSize: 16,
-    color: '#5c5e62'
-  }
+    subtitle: {
+        fontSize: 16,
+        color: '#5c5e62'
+    }
 });
