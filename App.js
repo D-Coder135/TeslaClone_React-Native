893e25f30f8506aca3f7690c1f8f345b555a38d2
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-27 21:24:51 +0530

    add the internal styling for the View tag

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import {StyleSheet, View} from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <View>
+      <View style={styles.carContainer}>
 
 
       </View>
