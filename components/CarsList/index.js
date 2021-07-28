c5fdaecde312375fb110a9a444406af535c9cef8
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-28 19:25:44 +0530

    return the CarItem tag inside the anonymous function of the renderItem property of the FlatList tag

--- components/CarsList/index.js
+++ components/CarsList/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import {View, Text, FlatList} from 'react-native';
 import styles from './styles';
 import cars from "./cars";
+import CarItem from "../CarItem";
 
 const CarList = (props) => {
     return (
         <View>
-            <FlatList data={cars} renderItem={(item) => {
-
-            }}/>
+            <FlatList data={cars} renderItem={({item}) => <CarItem car={item}/>}/>
         </View>
     );
 }
