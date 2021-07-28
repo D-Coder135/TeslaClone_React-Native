fc723ca32a6d65a6328f06c043873ea1b481aa2f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-28 21:34:18 +0530

    remove the props from the Header functional component

--- components/Header/index.js
+++ components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {View, Image} from 'react-native';
 import styles from './styles';
 
-const Header = (props) => {
+const Header = () => {
     return (
         <View style={styles.container}>
             <Image style={styles.logo} source={require('../../assets/images/logo.png')}/>
