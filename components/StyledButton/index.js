965f0843f6c9f77f5310bd6f9b2a4c8fb8fb6641
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-28 11:03:32 +0530

    update the color for the backgroundColor field

--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -6,7 +6,7 @@ const StyledButton = (props) => {
 
     const type = props.type;
 
-    const backgroundColor = type === 'primary' ? 'black' : 'white';
+    const backgroundColor = type === 'primary' ? '#171A20CC' : '#FFFFFFA6';
     const textColor = type === 'primary' ? 'white' : 'black';
 
     return (
