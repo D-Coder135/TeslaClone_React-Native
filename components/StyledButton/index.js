ce7f2cb2be258573c0b2b5c0cf79a984ec1b9a84
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-28 11:14:31 +0530

    delete some part of the code

--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -15,9 +15,7 @@ const StyledButton = (props) => {
         <View style={styles.container}>
             <Pressable
                 style={[styles.button, {backgroundColor: backgroundColor}]}
-                onPress={() => {
-                    console.warn("Hey there");
-                }}
+                onPress={}
             >
                 <Text style={[styles.text, {color: textColor}]}>
                     {content}
