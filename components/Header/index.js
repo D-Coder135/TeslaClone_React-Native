5b8278651caf40e460824bb9d8239cd0ac228b74
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-28 20:03:39 +0530

    add one more Image tag along with some fix ups.

--- components/Header/index.js
+++ components/Header/index.js
@@ -6,6 +6,8 @@ const Header = (props) => {
     return (
         <View style={styles.container}>
             <Image style={styles.logo} source={require('../../assets/images/logo.png')}/>
+            <Image style={styles.menu} source={require('../../assets/images/menu.png')}/>
+
         </View>
     );
 }
