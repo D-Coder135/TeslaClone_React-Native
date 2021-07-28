96c357365d8edf255465ec917fe683889a34ea63
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-28 20:12:54 +0530

    add the paddingHorizontal property

--- components/Header/styles.js
+++ components/Header/styles.js
@@ -8,6 +8,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         width: '100%',
         justifyContent: 'space-between',
+        paddingHorizontal: 10,
     },
 
     logo: {
