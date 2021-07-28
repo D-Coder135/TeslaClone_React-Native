db04afbb1fb02cf5bd4011d58fbd9b4eb69a70e3
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-28 21:36:13 +0530

    optimize imports

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {StyleSheet, Text, View, ImageBackground} from 'react-native';
-import CarItem from './components/CarItem';
+import {StyleSheet, View} from 'react-native';
 import CarsList from './components/CarsList';
 import Header from './components/Header';
 
