03df6154d53a2aedbc5a3bd8a721545b66b98f2b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-28 20:09:18 +0530

    add the position property for the container attribute

--- components/Header/styles.js
+++ components/Header/styles.js
@@ -1,7 +1,9 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {},
+    container: {
+        position: 'absolute',
+    },
 
     logo: {
         width: 100,
