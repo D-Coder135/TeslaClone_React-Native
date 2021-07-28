d1567951744af5307f9610fcfaca84862f9b2de1
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-28 11:30:07 +0530

    pass the image parameter to the CarItem tag

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ export default function App() {
             <CarItem
                 name={'Model S'}
                 tagline={'Starting from $69,540'}
+                image={require('./assets/images/ModelS.jpeg')}
             />
 
             <StatusBar style="auto"/>
