be3a1da41c923b8ff9b027054d12b0e89a6076ea
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-28 19:16:36 +0530

    render the CarsList component inside the View tag

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import {StatusBar} from 'expo-status-bar';
 import React from 'react';
 import {StyleSheet, Text, View, ImageBackground} from 'react-native';
 import CarItem from './components/CarItem';
-import CarsList from './components/CarsList'
+import CarsList from './components/CarsList';
 
 export default function App() {
     return (
         <View style={styles.container}>
+
+            <CarsList/>
 
             {/*<CarItem*/}
             {/*    name={'Model S'}*/}
